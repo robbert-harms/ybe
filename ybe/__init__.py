08ad6f3b2038798e8809fd0d3c3f3da76c8908d5
Robbert Harms <robbert@xkls.nl>
2020-04-07 19:51:54 +0200

    In between sync

--- ybe/__init__.py
+++ ybe/__init__.py
@@ -1,9 +1,22 @@
-# -*- coding: utf-8 -*-
-
 __author__ = 'Robbert Harms'
 __date__ = '2020-07-07'
 __email__ = 'robbert@xkls.nl'
 __license__ = "LGPL v3"
 __maintainer__ = "Robbert Harms"
 
-from .__version__ import VERSION, VERSION_STATUS, __version__
+from ybe.__version__ import VERSION, VERSION_STATUS, __version__
+
+
+'''
+#todo
+- raise exception when a multiple choice question does not have an answer marked as 'correct'
+- gui?
+- query functions
+- yaml reader and writer
+- write down format descriptions
+    - i.e. which kind of values are allows and which are not
+
+
+class Question
+...
+'''
