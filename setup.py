991b24c33f3aa8dff60cd4f1bf99fdc74eb7dfdd
Robbert Harms <robbert@xkls.nl>
2020-04-17 16:24:02 +0200

    Updates the Python version in the support files.

--- setup.py
+++ setup.py
@@ -82,9 +82,7 @@ info_dict = dict(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering'
     ],
     test_suite='tests',
